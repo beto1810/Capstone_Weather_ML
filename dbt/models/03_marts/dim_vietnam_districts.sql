4ce40f960112c7f162fd227170c70e8c90491582
Dat Nguyen <101379141+beto1810@users.noreply.github.com>
2025-06-06 10:34:15 +0700

    Add dbt CI workflow using GitHub Actions (#1)

--- dbt/models/03_marts/dim_vietnam_districts.sql
+++ dbt/models/03_marts/dim_vietnam_districts.sql
@@ -1,52 +1,60 @@
-
 {{ 
     config( materialized='incremental', 
     unique_key=['district_id'], 
     incremental_strategy='merge', 
     merge_update_columns=['province_id','district_name', 'district_name_vn', 'latitude', 'longitude', 'updated_at'] ) }}
 
-with transformed as ( 
-    select 
-            district_id,
-            province_id, 
-            district_name,
-            district_name_vn,
-            latitude, 
-            longitude, 
-            current_timestamp() as created_at, 
-            current_timestamp() as updated_at
-            from {{ ref('stg_vietnam_districts') }} ),
+with transformed as (
+    select
+        district_id,
+        province_id,
+        district_name,
+        district_name_vn,
+        latitude,
+        longitude,
+        current_timestamp() as created_at,
+        current_timestamp() as updated_at
+    from {{ ref('stg_vietnam_districts') }}
+),
 
 deduplicated as (
     select *
     from (
-        select *,
-            row_number() over (partition by district_id order by updated_at desc) as row_num
+        select
+            *,
+            row_number()
+                over (
+                    partition by district_id
+                    order by updated_at desc
+                )
+                as row_num
         from transformed
     )
     where row_num = 1
 )
 
-{% if is_incremental() %} 
+{% if is_incremental() %}
     -- Only process new or changed records 
-    select  distinct
-            deduplicated.district_id, 
-            deduplicated.province_id,
-            deduplicated.district_name,
-            deduplicated.district_name_vn,
-            deduplicated.latitude, 
-            deduplicated.longitude, 
-            coalesce(existing.created_at, deduplicated.created_at) as created_at, 
-            deduplicated.updated_at
-            from deduplicated  
-            left join {{ this }} as existing on deduplicated.district_id = existing.district_id 
-            where 
-                existing.district_id is null -- New records 
-                or deduplicated.province_id != existing.province_id
-                or deduplicated.district_name != existing.district_name 
-                or deduplicated.district_name_vn != existing.district_name_vn
-                or deduplicated.latitude != existing.latitude 
-                or deduplicated.longitude != existing.longitude 
+    select distinct
+        deduplicated.district_id,
+        deduplicated.province_id,
+        deduplicated.district_name,
+        deduplicated.district_name_vn,
+        deduplicated.latitude,
+        deduplicated.longitude,
+        deduplicated.updated_at,
+        coalesce(existing.created_at, deduplicated.created_at) as created_at
+    from deduplicated
+    left join
+        {{ this }} as existing
+        on deduplicated.district_id = existing.district_id
+    where
+        existing.district_id is null -- New records 
+        or deduplicated.province_id != existing.province_id
+        or deduplicated.district_name != existing.district_name
+        or deduplicated.district_name_vn != existing.district_name_vn
+        or deduplicated.latitude != existing.latitude
+        or deduplicated.longitude != existing.longitude
 {% else %} -- First run - process all records 
     select * from deduplicated 
 {% endif %}
--- dbt/models/03_marts/dim_vietnam_provinces.sql
+++ dbt/models/03_marts/dim_vietnam_provinces.sql
@@ -1,41 +1,43 @@
-
 {{ 
     config( materialized='incremental', 
     unique_key=['province_id'], 
     incremental_strategy='merge', 
     merge_update_columns=['province_name','province_name_vn', 'latitude', 'longitude', 'region','updated_at'] ) }}
 
-with transformed as ( 
-    select 
-            province_id, 
-            province_name,
-            province_name_vn, 
-            latitude, 
-            longitude, 
-            region,
-            current_timestamp() as created_at, 
-            current_timestamp() as updated_at
-            from {{ ref('stg_vietnam_provinces') }} )
+with transformed as (
+    select
+        province_id,
+        province_name,
+        province_name_vn,
+        latitude,
+        longitude,
+        region,
+        current_timestamp() as created_at,
+        current_timestamp() as updated_at
+    from {{ ref('stg_vietnam_provinces') }}
+)
 
-{% if is_incremental() %} 
+{% if is_incremental() %}
     -- Only process new or changed records 
-    select 
-            transformed.province_id, 
-            transformed.province_name, 
-            transformed.province_name_vn,
-            transformed.latitude, 
-            transformed.longitude, 
-            transformed.region,
-            coalesce(existing.created_at, transformed.created_at) as created_at, 
-            transformed.updated_at, 
-            from transformed 
-            left join {{ this }} as existing on transformed.province_id = existing.province_id 
-            where 
-                existing.province_id is null -- New records 
-                or transformed.province_name != existing.province_name 
-                or transformed.latitude != existing.latitude 
-                or transformed.longitude != existing.longitude 
-                or transformed.region != existing.region
+    select
+        transformed.province_id,
+        transformed.province_name,
+        transformed.province_name_vn,
+        transformed.latitude,
+        transformed.longitude,
+        transformed.region,
+        transformed.updated_at,
+        coalesce(existing.created_at, transformed.created_at) as created_at
+    from transformed
+    left join
+        {{ this }} as existing
+        on transformed.province_id = existing.province_id
+    where
+        existing.province_id is null -- New records 
+        or transformed.province_name != existing.province_name
+        or transformed.latitude != existing.latitude
+        or transformed.longitude != existing.longitude
+        or transformed.region != existing.region
 {% else %} -- First run - process all records 
     select * from transformed 
 {% endif %}
